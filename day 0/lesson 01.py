f2cc809e67092b02bc98e8a06fd60a8555117a8c
tazotamazashvili <tamazashvili400@gmail.com>
2025-02-10 18:35:30 +0400

    second commit

---
+++ day 0/lesson 01.py
@@ -0,0 +1,11 @@
+name = "tazo"
+surname = "tamazashvili"
+age = 15
+height = 175.4
+knows_programming = True
+is_Ugly = False
+likes_radiohead= True
+loves_music_in_general = True
+
+
+print(name + " " + surname + " " + str(age) + " " + str(height) + " " + str(knows_programming)+ " " + str(is_Ugly) + " " + str(likes_radiohead) + " " + str(loves_music_in_general) + " " + "<3")
