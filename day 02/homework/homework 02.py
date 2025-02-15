1a0d7dde879de8f56c462d2e6764e91e6d1d5636
tazotamazashvili <tamazashvili400@gmail.com>
2025-02-15 20:58:00 +0400

    second commit

---
+++ day 02/homework/homework 02.py
@@ -0,0 +1,35 @@
+# Homework 1
+first_string = "hello world"
+
+first_int = 78
+
+print("String:", first_string)
+print("Integer:", first_int)
+
+
+
+# Homework 2
+second_string = "გამარჯობა"
+second_string_repeat = "ელემენტი" * 10
+
+
+first = 1
+second = 999
+merge = int(first + second)
+
+
+gamarjoba = "hello "
+world = "world"
+hello_world = gamarjoba + world
+
+boolean_value = 7 * 3 - (2-3) + 7.2
+
+
+
+
+print(second_string)
+print(second_string_repeat)
+print(merge)
+print(hello_world)
+print(boolean_value)
+
