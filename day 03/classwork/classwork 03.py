af70e7169a7c1cece11d7f49680ba602b84d8223
tazotamazashvili <tamazashvili400@gmail.com>
2025-02-19 18:43:14 +0400

    third commit

---
+++ day 03/classwork/classwork 03.py
@@ -0,0 +1,16 @@
+#string, boolean, float, integer, None
+# input
+# variable
+# print
+# type casting
+
+# CLASSWORK 1
+
+
+
+input("flower")
+word1 = (input)
+input(3)
+num1 = (input)
+print(word1 * num1)
+
---
+++ day 03/homework/homework 03.py
@@ -0,0 +1,26 @@
+# დავალება 1
+
+number1 = input("chawere pirveli ricxvi: ")
+number2 = input("chawere meore ricxvi: ")
+number3 = input("chawere mesame ricxvi: ")
+number4 = input("chawere meotxe ricxvi: ")
+
+sul = (number1 + number2 + number3 + number4) / 4
+
+print("mathematical average of these numbers: " + str(sul))
+
+# დავალება 2
+
+firstname = input("Enter your first name: ")
+lastname = input("Enter your last name: ")
+
+print("mogesalmebit " + firstname + " " + lastname)
+
+# დავალება 3
+
+num5 = input("put in the first number: ")
+num6 = input("put in the second number: ")
+
+result = num5 + "😊" + num6
+
+print("result: " + result)
