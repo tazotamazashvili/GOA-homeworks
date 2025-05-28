00cc2286fc769049c724d6f0fdb23471557ad834
tazotamazashvili <tamazashvili400@gmail.com>
2025-05-28 15:54:00 +0400

    d29

---
+++ day 29/homework/hw29n3.py
@@ -0,0 +1,25 @@
+
+
+a = 12
+b = 3
+c = 8
+
+# Shedarebiti operaciebi
+print("a > b:", a > b)      # True - 12 metia 3-ze
+print("b == c:", b == c)    # False - 3 ar udris 8-s
+
+# Logikuri operaciebi
+print("True and False:", True and False)   # False - orive unda iyos True
+print("True or False:", True or False)     # True - ertis mainc True yofna sakmarisia
+
+# Sherwyvuli operacia
+result = (a > b) and (c < a) or (b == c)
+
+# Dashla:
+# (10 > 5) → True
+# (8 < 10) → True
+# (5 == 8) → False
+# True and True → True
+# True or False → True
+
+print("Shedegi:", result)  # True
---
+++ day 29/homework/hw29n4.py
@@ -0,0 +1,17 @@
+"""
+shedarebiti da logikuri operaciebi programirebashi dzalian gavs matematikashi arsebul logikas da gamotvlis wesebs
+pirvel rigshi shedarebiti operatorebi (magalitad: >, <, ==, !=) gamoiyeneba or mnishvnelobas shoris kavshiris shesamowmeblad
+isini abruneben logikur mnishvnelobas True or False
+
+logikuri operatorebi (and, or, not) sashualebas gvadzelvs ramdenime logikuri piroba shevaertot
+"and" abrunebs Trues mxolod mashin roca orive piroba Truea; 
+"or" abrunebs Trues, tu erti romelime piroba mainc aris True; 
+"not" abrunebs mocemuli pirobis sapirispiros
+
+operaciis shesruleba xdeba garkeveuli prioritetit rogorc matematikashi, magalitad:
+1. pirveli sruldeba prchxilebi
+2. shemdeg shedarebiti operatorebi (marcxnidan marjvniv rogorcaa dawerili imis mixedvit, mag: tu pirveli > weria mashin pirveli > shesruldeba da a.sh)
+3. shedarebitis shemdeg not radgan umaglesi prioriteti aqvs logikuri operatorebidan
+4. shemdeg and 
+5. bolos or
+"""
